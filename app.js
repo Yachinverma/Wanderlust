9f2dfcd85c9de5f04038168fda061e193e178e2d
Yachinverma <yachinverma@gmail.com>
2025-01-11 13:28:36 +0530

    New change

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-if(process.env.NODE_ENV !="production"){
-    require('dotenv').config();
-
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
 }
 // console.log(process.env.SECRETE) // remove this after you've confirmed it is working
 
@@ -8,42 +7,42 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
-const methodOverride=require("method-override");
+const methodOverride = require("method-override");
 const ejsmate = require("ejs-mate");
-const expressError= require("./utils/ExpressError.js");
+const expressError = require("./utils/ExpressError.js");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
-const  flash = require("connect-flash");
+const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user");
 
 const listingRouter = require("./routes/listing.js");
-const reviewRouter= require("./routes/review.js");
-const userRouter= require("./routes/user.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
-
-const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust';
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 const dbUrl = process.env.ATLASDB_URL;
-main().then(()=>{
-    console.log("Connection Successful")
-})
-.catch((err)=> console.log(err));
+main()
+  .then(() => {
+    console.log("Connection Successful");
+  })
+  .catch((err) => console.log(err));
 
 // async function main(){
 //     await mongoose.connect(dbUrl);
 // }
 
-async function main(){
-    await mongoose.connect(MONGO_URL);
+async function main() {
+  await mongoose.connect(MONGO_URL);
 }
 
-app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"views"));
-app.use(express.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.engine("ejs",ejsmate);
-app.use(express.static(path.join(__dirname,"/public")));
+app.engine("ejs", ejsmate);
+app.use(express.static(path.join(__dirname, "/public")));
 
 // const store = MongoStore.create({
 //     mongoUrl:dbUrl,
@@ -58,20 +57,19 @@ app.use(express.static(path.join(__dirname,"/public")));
 // });
 
 const sessionOptions = {
-    // store,
-    secret: process.env.SECRET,
-    resave: false,
-    saveUninitialized:true,
-    cookie:{
-        expires:Date.now() + 7 * 24 *60 * 60 * 1000,
-        maxAge: 7 * 24 *60 * 60 * 1000,
-        httpOnly:true,
-    }
+  // store,
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  },
 };
 
-app.get("/",(req,res)=>{
-    res.send("Hi! I am root.");
-    res.redirect("/listings");
+app.get("/", (req, res) => {
+  res.redirect("/listings");
 });
 
 app.use(session(sessionOptions));
@@ -84,11 +82,11 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use( (req,res,next) => {
-    res.locals.success = req.flash("success");
-    res.locals.error = req.flash("error");
-    res.locals.currUser= req.user;
-    next();
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  res.locals.currUser = req.user;
+  next();
 });
 
 // app.get("/demouser",async(req,res)=>{
@@ -100,21 +98,20 @@ app.use( (req,res,next) => {
 //     let registeredUser=await User.register(fakeUser,"helloworld");
 //     res.send(registeredUser);
 // })
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter);
 
-app.use("/listings",listingRouter);
-app.use("/listings/:id/reviews",reviewRouter);
-app.use("/",userRouter);
-
-app.all("*",(req,res,next)=>{
-    next(new expressError(404,"Page Not Found!"));
-})
-
-app.use((err,req,res,next)=>{
-    let {statusCode=500,message="Something went wrong"} = err;
-    res.status(statusCode).render("error.ejs",{message});
-    // res.status(statusCode).send(message);
+app.all("*", (req, res, next) => {
+  next(new expressError(404, "Page Not Found!"));
 });
 
-app.listen(8080,()=>{
-    console.log("server is listening to port 8080.");
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message = "Something went wrong" } = err;
+  res.status(statusCode).render("error.ejs", { message });
+  // res.status(statusCode).send(message);
+});
+
+app.listen(8080, () => {
+  console.log("server is listening to port 8080.");
 });
--- routes/review.js
+++ routes/review.js
@@ -1,19 +1,32 @@
 const express = require("express");
-const router = express.Router({mergeParams:true});
+const router = express.Router({ mergeParams: true });
 const WrapAsync = require("../utils/wrapAsync.js");
-const expressError= require("../utils/ExpressError.js");
+const expressError = require("../utils/ExpressError.js");
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
-const {validateReview,isLoggedin,isReviewAuthor} = require("../middleware.js");
+const {
+  validateReview,
+  isLoggedin,
+  isReviewAuthor,
+} = require("../middleware.js");
 
 const reviewController = require("../controllers/reviews.js");
 
 //Reviews
 //post Route
-router.post("/",isLoggedin,validateReview,WrapAsync(reviewController.createReview));
-   
-   
-   //Delete Review Route
-   router.delete("/:reviewId",isLoggedin,isReviewAuthor,WrapAsync(reviewController.destroyReview));
+router.post(
+  "/",
+  isLoggedin,
+  validateReview,
+  WrapAsync(reviewController.createReview)
+);
 
-   module.exports = router;
+//Delete Review Route
+router.delete(
+  "/:reviewId",
+  isLoggedin,
+  isReviewAuthor,
+  WrapAsync(reviewController.destroyReview)
+);
+
+module.exports = router;
