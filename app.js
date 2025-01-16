e9f32c0bc01eca9d22c4990a975d78fdb69b338f
Yachinverma <yachinverma@gmail.com>
2025-01-16 19:53:54 +0530

    secret spelling change

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 const dbUrl = process.env.ATLASDB_URL;
 main()
   .then(() => {
@@ -29,13 +29,13 @@ main()
   })
   .catch((err) => console.log(err));
 
-// async function main(){
-//     await mongoose.connect(dbUrl);
-// }
-
-async function main() {
-  await mongoose.connect(MONGO_URL);
+async function main(){
+    await mongoose.connect(dbUrl);
 }
+
+// async function main() {
+//   await mongoose.connect(MONGO_URL);
+// }
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -44,20 +44,20 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsmate);
 app.use(express.static(path.join(__dirname, "/public")));
 
-// const store = MongoStore.create({
-//     mongoUrl:dbUrl,
-//     crypto:{
-//         secrete:process.env.SECRET,
-//     },
-//     touchAfter:24*3600,
-// });
+const store = MongoStore.create({
+    mongoUrl:dbUrl,
+    crypto:{
+        secret:process.env.SECRET,
+    },
+    touchAfter:24*3600,
+});
 
-// store.on("error", ()=> {
-//     console.log("ERROR in MONGO SESSION STORE");
-// });
+store.on("error", ()=> {
+    console.log("ERROR in MONGO SESSION STORE");
+});
 
 const sessionOptions = {
-  // store,
+  store,
   secret: process.env.SECRET,
   resave: false,
   saveUninitialized: true,
